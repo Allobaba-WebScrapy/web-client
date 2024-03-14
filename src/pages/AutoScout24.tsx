67fb323729eadd1a711b2a07c79668ee836d8d35
ilorez <zobirnajdaoui@gmail.com>
2024-03-14 10:05:52 +0000

    team update

--- src/pages/AutoScout24.tsx
+++ src/pages/AutoScout24.tsx
@@ -5,6 +5,7 @@ import { useSelector ,useDispatch} from "react-redux";
 
 const AutoScout24 = () => {
     const url = useSelector((state: RootState) => state.autoscout24.url);
+    // const data = useSelector((state: RootState) => state.autoscout24.data);
     const dispatch = useDispatch<AppDispatch>();
     return (
         <div>
@@ -12,6 +13,7 @@ const AutoScout24 = () => {
             <input type="text" onChange={(e)=>dispatch(setUrl(e.target.value))}/>
 
             <div>{url}</div>
+            <button className="bg-red-200" >click to fetch</button>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{ts,js,jsx,tsx}',
+    './components/**/*.{ts,js,jsx,tsx}',
+    './app/**/*.{ts,tsx,js,jsx}',
+    './src/**/*.{ts,tsx,js,jsx}',
   ],
   prefix: "",
   theme: {
