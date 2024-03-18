e021fc61239d81c44e33e63ba4592aed6e78f3f9
ilorez <zobirnajdaoui@gmail.com>
2024-03-18 15:04:50 +0000

    edit navbar

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Link,NavLink } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+// import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
 
 const Navbar: React.FC = () => {
-    return (
-        <nav className='w-full h-fit px-2 py-8 bg-gray-700 text-white'>
-            <ul className='flex justify-between'>
-                <Link to='/'>Scrapy</Link>
-                <NavLink to='/'>Menu</NavLink>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="flex w-full h-full items-center justify-between  px-4 py-2 ">
+      <Link to="/">Scrapy</Link>
+      {/* <NavLink to='/'>Menu</NavLink> */}
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary">
+            <span className="sr-only">Menu</span>
+            scrapy pages
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <Link to="/scrapy/autoscout24">
+            <DropdownMenuItem>AutoScout24</DropdownMenuItem>
+          </Link>
+          <DropdownMenuSeparator />
+          <Link to="/scrapy/orange">
+            <DropdownMenuItem>Orange</DropdownMenuItem>
+          </Link>
+          <DropdownMenuSeparator />
+          <Link to="/scrapy/pagesjaunes">
+            <DropdownMenuItem>PagesJaunes</DropdownMenuItem>
+          </Link>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
+  );
 };
 
 export default Navbar;
