2ef1d83913fc4334117e9bc0b656249682234cbd
ilorez <zobirnajdaoui@gmail.com>
2024-03-25 18:04:57 +0000

    restyle delete selected products button

--- src/components/global/autoscout24/table_components/data-table.tsx
+++ src/components/global/autoscout24/table_components/data-table.tsx
@@ -9,7 +9,7 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-} from "@tanstack/react-table"
+} from "@tanstack/react-table";
 
 import {
   Table,
@@ -18,38 +18,37 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-
-import { Button } from "@/components/ui/button"
-import { useState } from "react"
-import { Input } from "@/components/ui/input"
-import { store } from "@/state/store"
-import { toast } from "@/components/ui/use-toast"
-import { removeSelectedProducts } from "@/state/autoscout24/AutoScout24Slice"
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { store } from "@/state/store";
+import { toast } from "@/components/ui/use-toast";
+import { removeSelectedProducts } from "@/state/autoscout24/AutoScout24Slice";
+import { Trash2 } from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
-    const [sorting, setSorting] = useState<SortingState>([])
-    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
-    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
-    const [rowSelection, setRowSelection] = useState({})
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
 
-  
   const table = useReactTable({
     data,
     columns,
@@ -67,38 +66,42 @@ export function DataTable<TData, TValue>({
       columnVisibility,
       rowSelection,
     },
-  })
+  });
   const deleteSelectedProducts = () => {
-    const selectedRows = table.getFilteredSelectedRowModel().rows
+    const selectedRows = table.getFilteredSelectedRowModel().rows;
     if (selectedRows.length === 0) {
-      alert('No Products Selected. Please select the products first.')
-      return
+      alert("No Products Selected. Please select the products first.");
+      return;
     }
-    const selectedProducts = selectedRows.map(row => JSON.stringify(row.original))
-    console.log(selectedProducts)
-    
-    store.dispatch(removeSelectedProducts(selectedProducts))
-    table.toggleAllPageRowsSelected(false)
-    toast({title:'Selected Products Deleted Successfully'})
-  }
+    const selectedProducts = selectedRows.map((row) =>
+      JSON.stringify(row.original)
+    );
+    console.log(selectedProducts);
+
+    store.dispatch(removeSelectedProducts(selectedProducts));
+    table.toggleAllPageRowsSelected(false);
+    toast({ title: "Selected Products Deleted Successfully" });
+  };
 
   return (
     <div>
-    <div className="flex items-center py-4">
-    <Input
+      <div className="flex items-center py-4">
+        <Input
           placeholder="Filter vendors..."
           value={(table.getColumn("vendor")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>{
+          onChange={(event) => {
             // console.log(table.getAllColumns())
-            console.log(table.getState().rowSelection) //get the row selection state - { 1: true, 2: false, etc... }
-console.log(table.getSelectedRowModel().rows) //get full client-side selected rows
-console.log(table.getFilteredSelectedRowModel().rows) //get filtered client-side selected rows
-console.log(table.getGroupedSelectedRowModel().rows)
-            return table.getColumn("vendor")?.setFilterValue(event.target.value)}
-          }
+            // console.log(table.getState().rowSelection) //get the row selection state - { 1: true, 2: false, etc... }
+            // console.log(table.getSelectedRowModel().rows) //get full client-side selected rows
+            // console.log(table.getFilteredSelectedRowModel().rows) //get filtered client-side selected rows
+            // console.log(table.getGroupedSelectedRowModel().rows)
+            return table
+              .getColumn("vendor")
+              ?.setFilterValue(event.target.value);
+          }}
           className="max-w-sm"
         />
-        <Button onClick={deleteSelectedProducts}>Delete</Button>
+
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="ml-auto">
@@ -108,9 +111,7 @@ console.log(table.getGroupedSelectedRowModel().rows)
           <DropdownMenuContent align="end">
             {table
               .getAllColumns()
-              .filter(
-                (column) => column.getCanHide()
-              )
+              .filter((column) => column.getCanHide())
               .map((column) => {
                 return (
                   <DropdownMenuCheckboxItem
@@ -123,82 +124,89 @@ console.log(table.getGroupedSelectedRowModel().rows)
                   >
                     {column.id}
                   </DropdownMenuCheckboxItem>
-                )
+                );
               })}
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-    <div className="flex space-x-2 py-4">
-
-    <div className="flex text-sm text-muted-foreground">
-  {table.getFilteredSelectedRowModel().rows.length} of{" "}
-  {table.getFilteredRowModel().rows.length} row(s) selected.
-</div>
-
-    <div className="flex flex-1 items-center justify-end ">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-          >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-          >
-          Next
-        </Button>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
       </div>
-            </div>
+      <div className="flex space-x-2 py-4">
+        <div className="flex text-sm text-muted-foreground items-center gap-4">
+          <Button onClick={deleteSelectedProducts} >
+            <Trash2 />
+          </Button>
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+
+        <div className="flex flex-1 items-center justify-end ">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
     </div>
-    
-  )
+  );
 }
