284071c31b7bb98e0d516cec6a182fafcd35c0fe
ilorez <zobirnajdaoui@gmail.com>
2024-03-27 16:07:21 +0000

    basic home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
-import { Button } from "./components/ui/button";
+import PageCard from "./components/global/PageCard";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      <Link to="/scrapy/autoscout24">
-        <Button>AutoScout24</Button>
-      </Link>
-      <Link to="/scrapy/orange">
-        <Button>Orange</Button>
-      </Link>
-      <Link to="/scrapy/pagesjaunes">
-        <Button>PagesJaunes</Button>
-      </Link>
-    </h1>
+    <div className="flex flex-col w-full h-[100vh] items-center justify-center ">
+      <div className="flex flex-col gap-5">
+        <div>
+          <h1 className="text-4xl font-bold">Scrapy</h1>
+          <p className="text-gray-500">Choose a website to scrape</p>
+        </div>
+        <div className="flex gap-4 aspect-1/3">
+          <PageCard btnValue="Autoscout24" imageUrl="/autoscout24.png" url="/scrapy/autoscout24" />
+          <PageCard btnValue="Orange" imageUrl="/orange.png" url="/scrapy/orange" />
+          <PageCard btnValue="PagesJaunes" imageUrl="/pagesjaunes.png" url="/scrapy/pagesjaunes" />
+        </div>
+      </div>
+    </div>
   );
 }
